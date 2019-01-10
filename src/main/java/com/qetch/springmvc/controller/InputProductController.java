d857c4772f75e922ba31c703eb2bcb1063eda85f
JiaFeiCat93 <1046709362@qq.com>
2019-01-10 19:19:00 +0800

    springmvc-app03a init

---
+++ src/main/java/com/qetch/springmvc/controller/InputProductController.java
@@ -0,0 +1,18 @@
+package com.qetch.springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+
+public class InputProductController implements Controller {
+	private static Log log = LogFactory.getLog(InputProductController.class);
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		log.info("InputProductController called");
+		return new ModelAndView("/WEB-INF/jsp/ProductForm.jsp");
+	}
+}

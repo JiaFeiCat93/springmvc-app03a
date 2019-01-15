7c90f52a5758a246072b6e05b457f2e6da0fa735
JiaFeiCat93 <37857620+JiaFeiCat93@users.noreply.github.com>
2019-01-16 00:59:59 +0800

    20190116 at home

--- src/main/java/com/qetch/springmvc/controller/InputProductController.java
+++ src/main/java/com/qetch/springmvc/controller/InputProductController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 public class InputProductController implements Controller {
 	private static Log log = LogFactory.getLog(InputProductController.class);
--- src/main/java/com/qetch/springmvc/controller/SaveProductController.java
+++ src/main/java/com/qetch/springmvc/controller/SaveProductController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.qetch.springmvc.domain.Product;
 import com.qetch.springmvc.form.ProductForm;
